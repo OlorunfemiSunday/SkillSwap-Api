e77fd6b72c1537a6a5ba2cc6afa8911be53f4334
Olorunfemi Sunday <105191911+OlorunfemiSunday@users.noreply.github.com>
2026-02-05 01:44:31 +0100

    user registration updated

--- models/User.js
+++ models/User.js
@@ -37,16 +37,15 @@ const userSchema = new mongoose.Schema(
 );
 
 // Pre-save hook to hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
   } catch (err) {
     console.error("Password hashing error:", err);
-    next(err);
+    throw err;
   }
 });
 
